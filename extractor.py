4f873ba18adea8c54ddc2f496f41e91b87eb5024
Srivatsan Srinivasan <srivatsan.srinivasan@cognizant.com>
2024-10-27 06:53:50 -0700

    Enhanced Extractor for better comparison function

--- extractor.py
+++ extractor.py
@@ -1,7 +1,7 @@
 import spacy
 import json
 from sklearn.metrics.pairwise import cosine_similarity
-from sector_helper import clean_text, replace_with_synonyms, lemmatize, lemmatize_dynamic, combine_sentences, combine_sentences_simple, nlp, jaccard_similarity
+from sector_helper import clean_text, is_sequential, replace_with_synonyms, lemmatize, lemmatize_dynamic, combine_sentences, combine_sentences_simple, nlp, jaccard_similarity, get_embedding, key_input_coverage, process_text, embed_process
 import itertools
 
 
@@ -14,7 +14,7 @@ import itertools
 #     union = len(set1.union(set2))
 #     return intersection / union
 
-def match_sentence(input_sentence, reference_sentences, max_window_size, use_semantic, combine_threshold=0.6, debug=False):
+def match_sentence(input_sentence, reference_sentences, max_window_size, use_semantic, combine_threshold, debug, search, embed_fn):
     """Matches an input sentence to a set of reference sentence combinations using both ordered and unordered methods."""
     best_match = None
     best_score = 0
@@ -38,65 +38,78 @@ def match_sentence(input_sentence, reference_sentences, max_window_size, use_sem
     for window_size in range(1, max_window_size + 1):
         for combination in itertools.combinations(range(len(reference_sentences)), window_size):
             # Try ordered combinations
-            ordered_combined = combine_sentences(reference_sentences, combination)
-            lemmatized_ref_ordered = lemmatize_dynamic(ordered_combined)
-            #lemmatized_ref_ordered = replace_with_synonyms(lemmatized_ref_ordered)
-
-            if use_semantic:
-                input_vector = nlp(" ".join(lemmatized_input)).vector.reshape(1, -1)
-                ref_vector_ordered = nlp(" ".join(lemmatized_ref_ordered)).vector.reshape(1, -1)
-                score_ordered = cosine_similarity(input_vector, ref_vector_ordered).flatten()[0]
-            else:
-                score_ordered = jaccard_similarity(set(lemmatized_ref_ordered), set(lemmatized_input))
-
-            if debug:
-                print(f"Ordered Combination: {ordered_combined}")
-                print(f"Similarity Score (Ordered): {score_ordered:.5f}")
-
-            if score_ordered > best_score:
-                best_match = ordered_combined
-                best_score = score_ordered
-                match_data["best_match"] = ordered_combined
-                match_data["best_score"] = score_ordered
-                match_data["combination_type"] = "ordered"
-                match_data["combination_size"] = window_size
-
-            # If the score exceeds the combine threshold, stop further merging
-            if score_ordered >= combine_threshold:
-                return match_data
-
-            # Try unordered combinations
-            for permutation in itertools.permutations(combination):
-                unordered_combined = combine_sentences(reference_sentences, permutation)
-                lemmatized_ref_unordered = lemmatize_dynamic(unordered_combined)
-                #lemmatized_ref_unordered = replace_with_synonyms(lemmatized_ref_unordered)
+            if (search == 'sequential' and is_sequential(combination)) or search == 'ordered':
+                ordered_combined = combine_sentences(reference_sentences, combination)
+                lemmatized_ref_ordered = lemmatize_dynamic(ordered_combined)
+                #lemmatized_ref_ordered = replace_with_synonyms(lemmatized_ref_ordered)
 
                 if use_semantic:
-                    ref_vector_unordered = nlp(" ".join(lemmatized_ref_unordered)).vector.reshape(1, -1)
-                    score_unordered = cosine_similarity(input_vector, ref_vector_unordered).flatten()[0]
+                    input_vector = embed_process(lemmatized_input, embed_fn=embed_fn)
+                    ref_vector_ordered = embed_process(lemmatized_ref_ordered, embed_fn=embed_fn)
+                    score_ordered = cosine_similarity(input_vector, ref_vector_ordered).flatten()[0]
                 else:
-                    score_unordered = jaccard_similarity(set(lemmatized_ref_unordered), set(lemmatized_input))
+                    #score_ordered = jaccard_similarity(set(lemmatized_ref_ordered.split()), set(lemmatized_input.split()))
+                    score_ordered = key_input_coverage(lemmatized_input, lemmatized_ref_ordered)
 
                 if debug:
-                    print(f"Unordered Combination: {unordered_combined}")
-                    print(f"Similarity Score (Unordered): {score_unordered:.5f}")
+                    print(f"Ordered Combination: {ordered_combined}")
+                    print(f"Similarity Score (Ordered): {score_ordered:.5f}")
 
-                if score_unordered > best_score:
-                    best_match = unordered_combined
-                    best_score = score_unordered
-                    match_data["best_match"] = unordered_combined
-                    match_data["best_score"] = score_unordered
-                    match_data["combination_type"] = "unordered"
+                if score_ordered > best_score:
+                    best_match = ordered_combined
+                    best_score = score_ordered
+                    match_data["best_match"] = ordered_combined
+                    match_data["best_score"] = score_ordered
+                    if search == 'sequential':
+                        match_data["combination_type"] = "sequential"
+                    else:
+                        match_data["combination_type"] = "ordered"
                     match_data["combination_size"] = window_size
 
                 # If the score exceeds the combine threshold, stop further merging
-                if score_unordered >= combine_threshold:
+                if score_ordered >= combine_threshold:
                     return match_data
+
+            # Try unordered combinations
+            if search in ('random'):
+                for permutation in itertools.permutations(combination):
+                    unordered_combined = combine_sentences(
+                        reference_sentences, permutation)
+                    lemmatized_ref_unordered = lemmatize_dynamic(
+                        unordered_combined)
+                    #lemmatized_ref_unordered = replace_with_synonyms(lemmatized_ref_unordered)
+
+                    if use_semantic:
+                        input_vector = embed_process(lemmatized_input, embed_fn=embed_fn)
+                        ref_vector_unordered = embed_process(lemmatized_ref_unordered, embed_fn=embed_fn)
+                        score_unordered = cosine_similarity(
+                            input_vector, ref_vector_unordered).flatten()[0]
+                    else:
+                        #score_unordered = jaccard_similarity(set(lemmatized_ref_unordered.split()), set(lemmatized_input.split()))
+                        score_ordered = key_input_coverage(lemmatized_input, lemmatized_ref_unordered)
+
+                    if debug:
+                        print(
+                            f"Unordered Combination: {unordered_combined}")
+                        print(
+                            f"Similarity Score (Unordered): {score_unordered:.5f}")
+
+                    if score_unordered > best_score:
+                        best_match = unordered_combined
+                        best_score = score_unordered
+                        match_data["best_match"] = unordered_combined
+                        match_data["best_score"] = score_unordered
+                        match_data["combination_type"] = "random"
+                        match_data["combination_size"] = window_size
+
+                    # If the score exceeds the combine threshold, stop further merging
+                    if score_unordered >= combine_threshold:
+                        return match_data
 
     return match_data
 
 
-def extract_similar_sentences(reference_document, input_text, max_window_size=2, use_semantic=False, combine_threshold=0.6):
+def extract_similar_sentences(reference_document, input_text, max_window_size=2, use_semantic=False, combine_threshold=0.6, debug=False, search='sequential', clean_fn=None, embed_fn=None):
     """
     Extracts the most relevant sentences from the reference document by matching each input sentence to all combinations of reference sentences.
     
@@ -107,8 +120,10 @@ def extract_similar_sentences(reference_document, input_text, max_window_size=2,
     :return: A list of JSON-like objects containing matching results.
     """
     # Clean the input and reference text
-    reference_document = clean_text(reference_document)
-    input_text = clean_text(input_text)
+    #reference_document = clean_text(reference_document)
+    #input_text = clean_text(input_text)
+    reference_document = process_text(reference_document, clean_fn=clean_fn)
+    input_text = process_text(input_text, clean_fn=clean_fn)
 
     # Tokenize reference document into sentences
     reference_doc = nlp(reference_document)
@@ -122,7 +137,7 @@ def extract_similar_sentences(reference_document, input_text, max_window_size=2,
 
     # Match input sentences to reference sentence combinations
     for input_sentence in input_sentences:
-        result = match_sentence(input_sentence, reference_sentences, max_window_size, use_semantic,combine_threshold)
+        result = match_sentence(input_sentence, reference_sentences, max_window_size, use_semantic,combine_threshold, debug, search, embed_fn)
         matched_sentences.append(result)
 
     return matched_sentences

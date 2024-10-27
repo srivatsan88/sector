7499655bcf7f57bb8450ecc3405d765e4f44150d
Srivatsan Srinivasan <srivatsan.srinivasan@cognizant.com>
2024-10-27 06:59:03 -0700

    Added additional parameters to customize pre-processing and embedding

--- sector.py
+++ sector.py
@@ -10,14 +10,12 @@ def process_json_list(json_list, top_n=3):
     results = []
 
     for entry in json_list:
-        print(entry)
         input_sentence = entry["input_sentence"]
         matched_reference = entry["best_match"]
 
         # Get matcher output for this input-reference pair
         matcher_result = json.loads(compare_sentences_flexible(input_sentence, matched_reference, top_n))
         results.append(matcher_result)
-        print(matcher_result)
 
         # Extract the individual scores for this entry
         scores = matcher_result["scores"]
@@ -35,6 +33,7 @@ def combine_scores(combined_scores, top_n=3):
         "levenshtein_similarity": 0,
         "pos_based_alignment_score": 0,
         "word_coverage": 0,
+        "key_word_coverage": 0,
         "geometric_mean_top_n": 0
     }
 
@@ -59,16 +58,18 @@ def combine_scores(combined_scores, top_n=3):
 
     return combined_result
 
-def run_sector (input_text, reference_document,  max_window_size=2,  use_semantic=True, combine_threshold=0.85, top_n_individual=2, top_n_aggregated=2):
+def run_sector (input_text, reference_document,  max_window_size=2,  use_semantic=True, combine_threshold=0.85, top_n_individual=2, top_n_aggregated=2, debug=False, search='sequential', clean_fn=None, embed_fn=None):
     similar_sentences_json = extract_similar_sentences(
     reference_document,
     input_text,
     max_window_size=max_window_size,  # Combine consecutive sentences if needed
     use_semantic=use_semantic,  # Set to True for semantic matching or False for simple sliding window
-    combine_threshold=combine_threshold  # Threshold for combining sentences
+    combine_threshold=combine_threshold,  # Threshold for combining sentences
+    debug=debug,
+    search=search,
+    clean_fn=clean_fn,
+    embed_fn=embed_fn
     )
-
-    print(similar_sentences_json)
 
     # Process the list of JSON objects
     combined_scores, results = process_json_list(similar_sentences_json, top_n_individual)
@@ -81,7 +82,7 @@ def run_sector (input_text, reference_document,  max_window_size=2,  use_semanti
         print(json.dumps(result, indent=2))
 
     # Output combined score result
-    print("\nCombined Scores across all input-reference pairs:")
+    #print("\nCombined Scores across all input-reference pairs:")
     #print(json.dumps(combined_result, indent=2))
     return combined_result
     
